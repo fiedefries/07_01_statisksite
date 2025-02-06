945b49dc203bf50b6159f90eb6f1ec387b9193c9
Fie de Fries <fijo0001@stud.kea.dk>
2025-02-06 09:07:49 +0100

    add discount og soldout

--- produkt.js
+++ produkt.js
@@ -1,29 +1,43 @@
-console.log("siden vises");
 const productId = new URLSearchParams(window.location.search).get("id");
-console.log("produktet", productId);
 const produktContainer = document.querySelector(".produktContainer");
-console.log("produktContainer:", produktContainer);
 
 fetch(`https://kea-alt-del.dk/t7/api/products/${productId}`)
   .then((response) => response.json())
-
-  .then((showProducts); 
-    
-    function showProducts(product){
-    const markup =  `
-        <div class="produktside">
-        <div>
-          <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="badedragt" />
-        </div>
+  .then((data) => {
+    produktContainer.innerHTML = `
+       
+    <div class="image_container ${data.soldout ? "soldOut" : ""}">
+          <img src="https://kea-alt-del.dk/t7/images/webp/640/${productId}.webp" alt="produkt" />
+           <span class="saleLabel ${data.discount && "saleOn_product"}">sale : ${data.discount} % </span>
+          <span class="soldOutLabel ${data.soldout && "soldOut_product"}">Sold out</span>
+          </div>
         <div class="produkttekst">
-          <h2>${product.productdisplayname}</h2>
-          <p class="produktPrice"><span >Pris:</span> ${product.price},-</p>
-          <p class="grey">${product.gender}</p>
-          <p>This single piece swimsuit is perfect for recreational swimmers looking for a getaway from their home and office chores, in the club or in the cool neighbourhood pool.</p>
-          <a href="produkt.html">ADD TO BAG</a>
-        </div>
-      </div> 
-  `;
+       <h2 class="product-title">${data.productdisplayname}</h2>
+          <h3 class="category">${data.category}</h3>
+          
+          <h4 class="brand"> ${data.brandname},-</h4>
+          <h4 class="produktPrice">
+  <span>Pris:</span> 
+  ${data.discount ? `<span class="dashed">${data.price},-</span> <span class="new-price">${(data.price * (1 - data.discount / 100)).toFixed(2)},-</span>` : `${data.price},-`}
+</h4>
+           
+              <div class="size_container">
+         <label for="size">Vælg en størrelse:</label>
+      <select name="size" id="size">
+        <option value="35">35</option>
+        <option value="36">36</option>
+        <option value="37">37</option>
+        <option value="38">38</option>
+      </select>
+      </div>
 
-    produktContainer.innerHTML = markup;
+       <button class="add-to-bag" ${data.soldout ? "disabled" : ""}>Læg i kurv</button>
+          </div>
+            
+       
+          </div>
+         
+  
+      
+  `;
   });
--- produktliste.js
+++ produktliste.js
@@ -1,30 +1,47 @@
-const mycategori = new URLSearchParams(window.location.search).get("category");
-console.log("siden vises", mycategori);
 const produktListe = document.querySelector(".produktliste");
-const ovserskrift = document.querySelector("h2");
 
-ovserskrift.innerHTML = mycategori;
+const getString = window.location.search;
 
-fetch(`https://kea-alt-del.dk/t7/api/products?category=${mycategori}`)
+const getSearch = new URLSearchParams(getString);
+
+const category = getSearch.get("category");
+
+fetch(`https://kea-alt-del.dk/t7/api/products?category=${category}&limit=5000`)
   .then((response) => response.json())
   .then((data) => showList(data));
 
-function showList(products) {
-  const markup = products
+function showList(data) {
+  console.log("products", data);
+  const markup = data
 
     .map(
       (product) => `
-       
-        <div class="box">
-          <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="dame med badedragt" />
-          <h3>${product.productdisplayname}</h3>
-          <p class="grey">Swimwear</p>
-          <p>${product.price}</p>
-          <a href="produkt.html">Add to bag</a>
-        </div>
+      <section class="box ${product.discount ? "OnSale" : ""}">
+  <a href="produkt.html?id=${product.id}">
+    <div class="image_container">
+      <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" 
+           alt="${product.productdisplayname}" 
+           style="${product.soldout ? "opacity: 0.4;" : ""}"/>
+
+      <!-- UDSOLGT Label -->
+       <span class="saleLabel ${product.discount && "saleOn_img"}">sale : ${product.discount} % </span>
+          <span class="soldOutLabel ${product.soldout && "soldOut_img"}">Sold out</span>
+    </div>
+  </a>
+
+  <p class="brand">${product.brandname}</p>
+  <h2>${product.productdisplayname}</h2>
+ <p>
+  ${product.discount ? `<span class="dashed">${product.price},00 DKK</span> <span class="new-price">${(product.price * (1 - product.discount / 100)).toFixed(2)},00 DKK</span>` : `${product.price},00 DKK`}
+</p>
+
+  <a href="produkt.html?id=${product.id}" class="add_to_bag">
+    <p>ADD TO BAG</p>
+  </a>
+</section>
       `
     )
     .join("");
-
+  console.log("markup", markup);
   produktListe.innerHTML = markup;
 }
